420e7a43e2dfef3967c911e0804e6b0de0f9e87c
micchavezs <mic.chavez@duocuc.cl>
2026-07-10 00:09:48 +0000

    Agregar integrantes.py

---
+++ integrantes.py
@@ -0,0 +1,10 @@
+integrantes = [
+    "Juan Arcos",
+    "Dorian Chavez",
+]
+
+print("Integrantes del equipo:")
+print("-----------------------")
+
+for integrante in integrantes:
+    print(integrante)
